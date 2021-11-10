1f5f4a9756931d9b42e7513671a19cf795950914
JeffCarroll58 <jeff@carrollengr.com>
2021-11-10 16:20:38 -0600

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+
+interface Language {
+  id: number;
+  name: string;
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'ang-select2';
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  languages: Language[] =[];
+  selectedLanguage=3;
+
+  constructor(){
+    this.languages = [
+      {id: 1, name: "Angular"},
+      {id: 2, name: "Typescript"},
+      {id: 3, name: "Javascript"},
+      {id: 4, name: "Java"},
+      {id: 5, name: "DotNet"}
+    ];
+
+
+  }
+
+  onAddLanguage(){
+    console.log('~ onAddLanguage');
+    const l: Language = {id: 6, name: "Python"}
+    this.languages.push(l);
+    this.selectedLanguage=6;
+    console.log(this.languages)
+  }
+
 }
